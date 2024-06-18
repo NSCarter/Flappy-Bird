5edf0110c103e1de46791256e8f8277ab6962d4f
Niamh Carter <niamh.carter13@gmail.com>
2024-06-18 17:53:05 +0100

    Track the high score

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -4,9 +4,19 @@ signal reset
 
 func _ready():
 	$Background/Score.text = str(Stats.get_score())
+	$Background/HighScore.text = str(Stats.high_score)
+	
+	if Stats.save_high_score:
+		_save()
 
 
 func _on_button_pressed():
 	get_tree().reload_current_scene()
 	Stats.set_score(0)
 	queue_free()
+
+
+func _save():
+	var save_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	save_file.store_string(str(Stats.high_score))
+	Stats.save_high_score = false
--- Scripts/Stats.gd
+++ Scripts/Stats.gd
@@ -2,10 +2,20 @@ extends Node
 
 signal score_changed(score)
 var _score = 0
+var high_score = 0
+var save_high_score = false
+
+func _ready():
+	var save_file = FileAccess.open("user://savegame.save", FileAccess.READ)
+	high_score = int(save_file.get_as_text())
 
 func set_score(score):
 	_score = score
 	score_changed.emit(score)
+	
+	if score > high_score:
+		high_score = score
+		save_high_score = true
 	
 func get_score():
 	return _score
